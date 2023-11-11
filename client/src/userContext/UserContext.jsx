e0b3dd79049cbf9f2b392bfa3c01438e2d0a4a46
nkchambers <nkchames@gmail.com>
2023-11-10 21:56:56 -0800

    added /api to UserContext

--- client/src/userContext/UserContext.jsx
+++ client/src/userContext/UserContext.jsx
@@ -10,7 +10,7 @@ export function UserContextProvider({ children }) {
 
     useEffect(() => {
         if (!user) {
-            axios.get('/profile')
+            axios.get('/api/profile')
                 .then(({ data }) => {
                     setUser(data);
                     setReady(true);
